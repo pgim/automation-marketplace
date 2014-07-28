e94bf180c05e840b961ee2318b053cd668efd82c
Pablo Gimenez <pgim@outlook.com>
2014-07-28 17:20:15 -0300

    Set resources test folder for Chrome driver.

--- src/test/java/com/example/automation/util/Consts.java
+++ src/test/java/com/example/automation/util/Consts.java
@@ -4,9 +4,7 @@ public final class Consts {
 
 	public static final String USERNAME = "test-at@hotmail.com";
 	
-	public static final String PASSWORD = "test-at";
-	
-	public static final String CHROME_DRIVER = "C:\\Documentos\\tmp\\chromedriver.exe";
+	public static final String PASSWORD = "test-at";	
 	
 	public static final int TIMEOUT = 30;
 	
